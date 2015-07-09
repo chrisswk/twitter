26cff237c8720a999ae2555bd233704958e5cf04
Christopher William Kramlich <chris@kramlich.com>
2015-07-09 14:53:10 -0700

    Adding authentication to user model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
-  has_many :tweets
+    has_many :tweets
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end

f2dbc5764f79aa89f616e1d4746271d2ad951701
Harvey Ngo <harveydngo@gmail.com>
2015-07-09 15:48:28 -0700

    add signup form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,23 @@
-get '/' do  
+# get '/' do  
+# 	@tweets = Tweet.all
+# 	@user = User.find_by(id: 1)
+# 	erb :"users/index"	
+# end
+
+get '/' do
+
+	erb :index
+end
+
+
+get '/logout' do
+	session[:user_id] = nil
+	redirect '/'
+end
+
+post '/' do
 	@tweets = Tweet.all
-	@user = User.find_by(id: 1)
-	erb :"users/index"	
+	@user = User.create(handle: "@"+params[:handle], email: params[:email], password_hash: params[:password_hash])
+	session[:user_id] = @user.id
+	erb :"users/index"
 end
